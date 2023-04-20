a5eed2e15307941d7f3a7814432409e6d74627a7
Straviux <arvin.heredero@gmail.com>
2023-04-20 10:32:36 +0800

    ....

---
+++ assets/Home-5655b0e3.js
@@ -0,0 +1,1 @@
+import{_ as t,r as s,o as c,c as r,a as e,b as n}from"./index-78820169.js";const a={},_={class:"relative"},d={class:"bg-white rounded w-full"};function i(l,u){const o=s("router-view");return c(),r("div",_,[e("div",d,[e("div",null,[n(o)])])])}const m=t(a,[["render",i]]);export{m as default};
